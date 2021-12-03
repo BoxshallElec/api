d0afc853d652e3d576535b3cef5110b4261e3ae0
shw97 <shwethasrinivasan97@gmail.com>
2021-12-03 12:06:22 +1100

    date for key for intuit

--- utils/util.js
+++ utils/util.js
@@ -5,7 +5,9 @@ const intuit_config = config.get("intuit-config");
 function isValidPayload(signature, payload) {
 	var hash = crypto.createHmac('sha256', intuit_config.webhooksverifier).update(payload).digest('base64');
 	if (signature === hash) {
+		console.log("HELLO");
 		return true;
+		
 	}
 	return false;
 }
